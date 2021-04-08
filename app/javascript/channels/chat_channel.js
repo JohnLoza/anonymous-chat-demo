e9d412e27036b469edde4fd2134c0905595769ef
Erick Loza <lozabucio.jony@gmail.com>
2021-04-08 09:58:11 -0500

    Refactor

--- app/javascript/channels/chat_channel.js
+++ app/javascript/channels/chat_channel.js
@@ -3,15 +3,15 @@ import consumer from './consumer';
 if (!window.openedChats)
   window.openedChats = [];
 
-window.subscribeToChat = (chat_name) => {
-  console.log("-> subscribing to chat: ", chat_name);
-  const conn = consumer.subscriptions.create({ channel: "ChatChannel", room: chat_name }, {
+window.subscribeToChat = (chatRoom) => {
+  const conn = consumer.subscriptions.create({ channel: "ChatChannel", room: chatRoom }, {
     connected() {
-      console.log('-> Connection established for channel: ', chat_name);
+      console.log('-> Connection established for channel: ', chatRoom);
+      window.openedChats.push(conn);
     },
 
     disconnected() {
-      console.log('-> Disconected from: ', chat_name);
+      console.log('-> Disconnected from channel: ', chatRoom);
     },
 
     received(data) {
@@ -20,28 +20,40 @@ window.subscribeToChat = (chat_name) => {
     },
 
     appendLine(data) {
-      const element = document.querySelector(`[data-chat-room='${chat_name}']`)
+      const element = document.querySelector(`[data-chat-room='${chatRoom}']`)
       element.insertAdjacentHTML("beforeend", data.html)
     },
 
     removeEmptyMessage() {
-      const query = `[data-chat-room="${chat_name}"] .empty-chat`;
+      const query = `[data-chat-room="${chatRoom}"] .empty-chat`;
       const element = document.querySelector(query);
       if (element) {
         element.remove();
       }
     }
   });
-
-  window.openedChats.push(conn);
 }
 
 document.addEventListener("turbolinks:load", () => {
   const chatContainer = document.querySelector('[data-chat-room]');
-  if (chatContainer == null)
+  if (chatContainer == null) return;
+
+  if (weAreNotConnectedTo(chatContainer.dataset.chatRoom))
+    subscribeToChat(chatContainer.dataset.chatRoom);
+});
+
+document.addEventListener("turbolinks:before-render", () => {
+  // this code will run twice if the requested page is cached by Turbolinks
+  if (openedChats.length == 0)
     return;
 
-  const chatRoom = chatContainer.dataset.chatRoom;
+  openedChats.forEach(conn => {
+    conn.unsubscribe();
+  });
+  openedChats = [];
+});
+
+function weAreNotConnectedTo(chatRoom) {
   let chatConnection = null;
 
   openedChats.forEach(conn => {
@@ -50,8 +62,5 @@ document.addEventListener("turbolinks:load", () => {
       chatConnection = conn;
   });
 
-  if (chatConnection != null)
-    return;
-
-  subscribeToChat(chatContainer.dataset.chatRoom);
-});
+  return !chatConnection;
+}
